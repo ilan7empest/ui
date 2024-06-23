af56b6582c36f290d470f13348da4bc90de7c66f
ilan7empest <therecanbeonly1.1984@gmail.com>
2024-06-23 14:08:48 +0200

    Fix [Metrics] Move metrics filter beneath tabs

--- src/components/FunctionsPage/functions.util.js
+++ src/components/FunctionsPage/functions.util.js
@@ -216,14 +216,8 @@ export const generateActionsMenu = (
   apiGateways
 ) => {
   const functionIsDeleting = isFunctionDeleting(func, deletingFunctions)
-  const getFullFunction = (funcMin) => {
-    return chooseOrFetchFunction(
-      selectedFunction,
-      dispatch,
-      fetchFunction,
-      funcMin,
-      apiGateways
-    )
+  const getFullFunction = funcMin => {
+    return chooseOrFetchFunction(selectedFunction, dispatch, fetchFunction, funcMin, apiGateways)
   }
 
   return [
@@ -263,17 +257,17 @@ export const generateActionsMenu = (
           !FUNCTIONS_EDITABLE_STATES.includes(func?.state?.value)
       },
       {
+        label: 'View YAML',
+        icon: <Yaml />,
+        disabled: functionIsDeleting,
+        onClick: funcMin => getFullFunction(funcMin).then(func => toggleConvertedYaml(func))
+      },
+      {
         label: 'Delete',
         icon: <Delete />,
         className: 'danger',
         disabled: functionIsDeleting,
         onClick: onRemoveFunction
-      },
-      {
-        label: 'View YAML',
-        icon: <Yaml />,
-        disabled: functionIsDeleting,
-        onClick: (funcMin) => getFullFunction(funcMin).then((func) => toggleConvertedYaml(func))
       }
     ],
     [
